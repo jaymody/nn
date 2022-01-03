5270bb6f961b88aa7c3045f290ee3fd3b7c6dce9
jaymody <jaykmody@gmail.com>
2022-01-03 01:12:14 -0500

    Update/fix some things with mlp.py.

--- mlp.py
+++ mlp.py
@@ -15,34 +15,37 @@ class MLP(nn.Module):
         return x
 
 
-def train_fn(model, train_loader, criterion, optimizer):
+def train_fn(model, train_loader, criterion, optimizer, device):
     model.train()
-    train_loss = 0
+    running_loss = 0
+    n_total = 0
     for batch in train_loader:
         # load data to device
         x, y = batch
         x = x.to(device)
         y = y.to(device)
-        batch_size = x.size(0)
+        batch_size = x.shape[0]
 
         # feed forwards
         output = model(x)
 
         # compute loss
         loss = criterion(output, y)
-        train_loss += loss.item() / batch_size
+        running_loss += loss.item() * batch_size
+        n_total += batch_size
 
         # backprop and step
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
+    train_loss = running_loss / n_total
     return train_loss
 
 
-def eval_fn(model, val_loader, criterion):
+def eval_fn(model, val_loader, criterion, device):
     model.eval()
-    val_loss = 0
+    running_loss = 0
     n_correct = 0
     n_total = 0
     for batch in val_loader:
@@ -50,7 +53,7 @@ def eval_fn(model, val_loader, criterion):
         x, y = batch
         x = x.to(device)
         y = y.to(device)
-        batch_size = x.size(0)
+        batch_size = x.shape[0]
 
         # feed forward
         with torch.no_grad():
@@ -58,7 +61,7 @@ def eval_fn(model, val_loader, criterion):
 
         # compute loss
         loss = criterion(output, y)
-        val_loss += loss.item() / batch_size
+        running_loss += loss.item() * batch_size
 
         # get predictions
         pred = torch.argmax(output, axis=-1)
@@ -67,17 +70,21 @@ def eval_fn(model, val_loader, criterion):
         n_correct += torch.sum(pred == y).item()
         n_total += batch_size
 
+    val_loss = running_loss / n_total
     val_acc = n_correct / n_total
     return val_loss, val_acc
 
 
-if __name__ == "__main__":
+def main():
     # setup
     import random
+
     import numpy as np
     from torch.utils.data import DataLoader, random_split
     from torchvision.datasets import MNIST
-    from torchvision.transforms import Compose, ToTensor, Lambda
+    from torchvision.transforms import Compose, Lambda, ToTensor
+
+    from utils import get_device, set_seed, train_test_split_dataset
 
     # hyperparams
     lr = 1e-3
@@ -86,20 +93,16 @@ if __name__ == "__main__":
     num_workers = 4
     batch_size = 128
     train_test_ratio = 0.8
-    seed = 1234
+    seed = 123
 
     # get device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = get_device()
 
     # set seed for deterministic results
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+    set_seed(seed)
 
     # load datasets
-    transform = Compose([ToTensor(), Lambda(lambda x: torch.flatten(x))])
+    transform = Compose([ToTensor(), Lambda(torch.flatten)])
     train_dataset = MNIST(
         root="data/mnist/train", train=True, download=True, transform=transform
     )
@@ -108,9 +111,9 @@ if __name__ == "__main__":
     )
 
     # train/validation split
-    train_size = int(len(train_dataset) * train_test_ratio)
-    val_size = len(train_dataset) - train_size
-    train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
+    train_dataset, val_dataset = train_test_split_dataset(
+        train_dataset, train_test_ratio
+    )
 
     # data loaders
     train_loader = DataLoader(
@@ -151,10 +154,10 @@ if __name__ == "__main__":
     # train loop
     for epoch in range(n_epochs):
         # train
-        train_loss = train_fn(model, train_loader, criterion, optimizer)
+        train_loss = train_fn(model, train_loader, criterion, optimizer, device)
 
         # eval
-        val_loss, val_acc = eval_fn(model, val_loader, criterion)
+        val_loss, val_acc = eval_fn(model, val_loader, criterion, device)
 
         # log results
         print(
@@ -165,5 +168,9 @@ if __name__ == "__main__":
         )
 
     # test
-    _, test_acc = eval_fn(model, test_loader, criterion)
+    _, test_acc = eval_fn(model, test_loader, criterion, device)
     print(f"\nAccuracy on Test Set: {test_acc:.3f}")
+
+
+if __name__ == "__main__":
+    main()
